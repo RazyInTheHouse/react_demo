143f02de5f2e641669aa2c8bc3965884b833f847
Rex Huang <rexhuang@RexdeMacBook-Pro.local>
2024-03-05 17:41:06 +0800

    login page

--- src/View/login.js
+++ src/View/login.js
@@ -3,7 +3,7 @@ import { useFlowLogin } from '../hook/useApi'
 
 const LoginView = () => {
     const { login } = useFlowLogin()
-    const [empNo, setEmpNo] = useState('FB6693')
+    const [empNo, setEmpNo] = useState('')
     const [majaja, setMajaja] = useState('Skl03458')
 
     const handleLogin = async () => {
@@ -12,13 +12,29 @@ const LoginView = () => {
         }
         await login(input)
     }
+
     return (
-        <div style={{ marginTop: "24px" }}>
-
-            <span>帳號</span><input type='text' className='form-control form-control-3-1' value={empNo} onChange={(e) => setEmpNo(e.target.value)} />
-
-            <button onClick={handleLogin}>login</button>
+        <div className="container">
+            <div style={{ marginBottom: "24px" }}>
+                <h1>印刷品申請系統</h1>
+            </div>
+            
+            <div id="login">
+                <div className="login-form">
+                    <span className="fa fa-user"></span>
+                    <input
+                        autoFocus
+                        type="account"
+                        placeholder="電腦編號"
+                        value={empNo}
+                        onChange={(e) => setEmpNo(e.target.value)}
+                        required
+                    />
+                    <input type="button" value="登入" onClick={handleLogin}/>
+                </div>
+            </div>
         </div>
+        
     )
 }
 
